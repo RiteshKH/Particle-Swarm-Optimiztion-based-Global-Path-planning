eb0c8b13e8435a550166a20d9a2cca18e2630d01
RiteshKH <38212000+RiteshKH@users.noreply.github.com>
2019-09-16 14:06:59 +0530

    Added main function, constants' dependencies

--- PSO.cpp
+++ PSO.cpp
@@ -23,21 +23,14 @@
 
 using namespace std;
 
-int main()
+void particleSwarm(int argc, char **argv)
 {
-    //Initial and Control Parameters
-    int swarm_size = 100;  //Swarm Size
-    int no_of_iters = 500; //Number of Iterations
-    double w;              //Inertial Weight
-    double wmin = 0.2;
-    double wmax = 0.7;
-    double c1 = 2, c2 = 2; //acceleration coefficients
-    Coord target;          // Goal to reach
-    target.x = -500.0;
-    target.y = -1000.0;
+    Coord target; // Goal to reach
+    target.x = DESTINATION_X;
+    target.y = DESTINATION_Y;
     Coord start;
-    start.x = 1000.0;
-    start.y = 1000.0;
+    start.x = START_X;
+    start.y = START_Y;
 
     //Variables Initialization
     //Random Numbers used for updating position and velocity of particles
@@ -47,14 +40,14 @@ int main()
     double lower_bound = -1;
     double upper_bound = 1;
 
-    double *position_x = generate_random(swarm_size, lower_bound, upper_bound, seed++);
-    double *position_y = generate_random(swarm_size, lower_bound, upper_bound, seed++);
-    double *velocity_x = generate_random(swarm_size, lower_bound, upper_bound, seed++);
-    double *velocity_y = generate_random(swarm_size, lower_bound, upper_bound, seed++);
+    double *position_x = generate_random(SWARM_SIZE, lower_bound, upper_bound, seed++);
+    double *position_y = generate_random(SWARM_SIZE, lower_bound, upper_bound, seed++);
+    double *velocity_x = generate_random(SWARM_SIZE, lower_bound, upper_bound, seed++);
+    double *velocity_y = generate_random(SWARM_SIZE, lower_bound, upper_bound, seed++);
 
     // Structure to store random data for position and velocities
-    Coord States[swarm_size];
-    for (int i = swarm_size; i--;)
+    Coord States[SWARM_SIZE];
+    for (int i = SWARM_SIZE; i--;)
     {
         States[i].x = position_x[i];
         States[i].y = position_y[i];
@@ -63,22 +56,19 @@ int main()
     }
 
     // New Initializations
-    Coord particle_states[swarm_size];   //position and velocities of particles
-    double particle_fitness[swarm_size]; //fitness of particles
-    Coord local_best_pos[swarm_size];    //local best position of each particle across iterations
-    double local_best_fit[swarm_size];   //local best fitness of each particle across iterations
+    Coord particle_states[SWARM_SIZE];   //position and velocities of particles
+    double particle_fitness[SWARM_SIZE]; //fitness of particles
+    Coord local_best_pos[SWARM_SIZE];    //local best position of each particle across iterations
+    double local_best_fit[SWARM_SIZE];   //local best fitness of each particle across iterations
     Coord global_best_pos;               //global best position across all particles across iterations
     double global_best_fit;
-    // Boundary conditons for particle initialisations
-    double lower_boundary = -1500.0;
-    double upper_boundary = 1500.0;
 
     //Initialize all arrays to 0
     global_best_pos.x = start.x;
     global_best_pos.y = start.y;
 
-    int cols = swarm_size + 1;
-    int rows = no_of_iters + 1;
+    int cols = SWARM_SIZE + 1;
+    int rows = NO_OF_ITERS + 1;
     int initial_value = 0;
 
     int counter = 1;
@@ -98,27 +88,22 @@ int main()
     vector<vector<double>> local_pos_y;
     local_pos_y.resize(rows, vector<double>(cols, initial_value));
 
-    const double V_max = (upper_boundary - lower_boundary) / 10;
-    global_best_fit = fitness(global_best_pos, target, global_pos[0]);
-    double pos_multiple = (upper_boundary - lower_boundary) / 2;
-    double vel_multiple = (upper_boundary - lower_boundary) / 20;
+    global_best_fit = fitness(global_best_pos, global_pos[0]);
 
     // ==================================================================================//
     // Initialize obstacle parameters (Obstacles at random positions)
-    const int num_obstacle = 7;
-    bool incircle[num_obstacle];
-    double r = 200.0;
+    bool incircle[NUM_OBSTACLE];
     // double center_x[num_obstacle] = {-600, -300, 550, 600, 250};
     // double center_y[num_obstacle] = {-100, 500, 500, -200, -1000};
-    double center_x[num_obstacle];
-    double center_y[num_obstacle];
-    for (int i = num_obstacle; i--;)
+    double center_x[NUM_OBSTACLE];
+    double center_y[NUM_OBSTACLE];
+    for (int i = NUM_OBSTACLE; i--;)
     {
     init:
-        int c_x = (pos_multiple - 200) * (generate_random(1, -1, 1, seed++)[0]);
-        int c_y = (pos_multiple - 200) * (generate_random(1, -1, 1, seed++)[0]);
-        bool stinobs = inCircle(start.x, start.y, c_x, c_y, r);
-        bool tarinobs = inCircle(target.x, target.y, c_x, c_y, r);
+        int c_x = (POS_MULTIPLE - 200) * (generate_random(1, -1, 1, seed++)[0]);
+        int c_y = (POS_MULTIPLE - 200) * (generate_random(1, -1, 1, seed++)[0]);
+        bool stinobs = inCircle(start.x, start.y, c_x, c_y, R);
+        bool tarinobs = inCircle(target.x, target.y, c_x, c_y, R);
         if (!stinobs && !tarinobs)
         {
             center_x[i] = c_x;
@@ -126,7 +111,7 @@ int main()
         }
         else
         {
-            cout << "start or target in obstacle....." << endl;
+            cout << "start or target in obstacle..... Replanning" << endl;
             goto init;
         }
     }
@@ -135,46 +120,46 @@ int main()
     /**
 	 * First Iteration. Initialize swarm with Random Position and Velocity vectors for Each Particle.
 	 */
-    double thresh = (r * 0.1);
-    for (int i = swarm_size; i--;)
+    double thresh = (R * 0.1);
+    for (int i = SWARM_SIZE; i--;)
     {
         bool final_val_init = false;
         int ct = 500;
         do
         {
-            particle_states[i].x = pos_multiple * (States[i].x);
-            particle_states[i].y = pos_multiple * (States[i].y);
+            particle_states[i].x = POS_MULTIPLE * (States[i].x);
+            particle_states[i].y = POS_MULTIPLE * (States[i].y);
             double new_x = generate_random(1, 0, 1, seed++)[0];
             States[i].x = new_x;
             double new_y = generate_random(1, 0, 1, seed++)[0];
             States[i].y = new_y;
-            if (particle_states[i].x > upper_boundary)
-                particle_states[i].x = upper_boundary;
-            if (particle_states[i].y > upper_boundary)
-                particle_states[i].y = upper_boundary;
-            if (particle_states[i].x < lower_boundary)
-                particle_states[i].x = lower_boundary;
-            if (particle_states[i].y < lower_boundary)
-                particle_states[i].y = lower_boundary;
+            if (particle_states[i].x > UPPER_BOUNDARY)
+                particle_states[i].x = UPPER_BOUNDARY;
+            if (particle_states[i].y > UPPER_BOUNDARY)
+                particle_states[i].y = UPPER_BOUNDARY;
+            if (particle_states[i].x < LOWER_BOUNDARY)
+                particle_states[i].x = LOWER_BOUNDARY;
+            if (particle_states[i].y < LOWER_BOUNDARY)
+                particle_states[i].y = LOWER_BOUNDARY;
 
             //===================================================================================================//
             // OBSTACLE AVOIDANCE MODULE
-            final_val_init = obstacle_avoidance(num_obstacle, particle_states[i], start, center_x, center_y, r, thresh, seed, lower_boundary, upper_boundary);
+            final_val_init = obstacle_avoidance(NUM_OBSTACLE, particle_states[i], start, center_x, center_y, R, thresh, seed, LOWER_BOUNDARY, UPPER_BOUNDARY);
             //===================================================================================================//
             ct--;
 
         } while ((final_val_init) && (ct != 0));
 
-        particle_states[i].velx = vel_multiple * (States[i].x);
-        particle_states[i].vely = vel_multiple * (States[i].y);
+        particle_states[i].velx = VEL_MULTIPLE * (States[i].x);
+        particle_states[i].vely = VEL_MULTIPLE * (States[i].y);
 
         // Representing fitness function as the distance between the particles and local best
-        particle_fitness[i] = fitness(particle_states[i], target, global_pos[0]);
+        particle_fitness[i] = fitness(particle_states[i], global_pos[0]);
         local_best_pos[i].x = particle_states[i].x;
         local_best_pos[i].y = particle_states[i].y;
         local_best_fit[i] = particle_fitness[i];
 
-        if (local_best_fit[i] < global_best_fit)
+        if (local_best_fit[i] > global_best_fit)
         {
             global_best_pos.x = particle_states[i].x;
             global_best_pos.y = particle_states[i].y;
@@ -197,11 +182,11 @@ int main()
 	 * Calculations from Second iteration Onwards
 	 */
     int flag = 1, cnt = 0;
-    for (int iter_ctr = 2; iter_ctr <= no_of_iters; iter_ctr++)
+    for (int iter_ctr = 2; iter_ctr <= NO_OF_ITERS; iter_ctr++)
     {
 
         //Updating Velocity - Applying Psycho-Social Criteria
-        for (int i = swarm_size; i--;)
+        for (int i = SWARM_SIZE; i--;)
         {
             bool final_val = false;
             int ct = 500;
@@ -211,35 +196,35 @@ int main()
                 double rg = generate_random(1, 0, 1, seed++)[0];
                 // w = wmax - (((wmax - wmin) / (no_of_iters)) * iter_ctr);
                 // following update from "Intelligent Vehicle Global Path Planning Based on Improved Particle Swarm Optimization"
-                double c = 2.3;
-                w = wmin + (wmax - wmin) * exp(-pow(((c * iter_ctr) / no_of_iters), 2.0));
+                double w, c = 2.3; //Inertial Weight, constant
+                w = WMIN + (WMAX - WMIN) * exp(-pow(((c * iter_ctr) / NO_OF_ITERS), 2.0));
                 // cout << "w value : " << w << endl;
-                particle_states[i].velx = (w * particle_states[i].velx + c1 * rp * (local_best_pos[i].x - particle_states[i].x)) + c2 * rg * (global_best_pos.x - particle_states[i].x);
-                particle_states[i].vely = (w * particle_states[i].vely + c1 * rp * (local_best_pos[i].y - particle_states[i].y)) + c2 * rg * (global_best_pos.y - particle_states[i].y);
-                if (particle_states[i].velx > V_max)
-                    particle_states[i].velx = V_max;
-                if (particle_states[i].vely > V_max)
-                    particle_states[i].vely = V_max;
-                if (particle_states[i].velx < -V_max)
-                    particle_states[i].velx = -V_max;
-                if (particle_states[i].vely < -V_max)
-                    particle_states[i].vely = -V_max;
+                particle_states[i].velx = (w * particle_states[i].velx + C1 * rp * (local_best_pos[i].x - particle_states[i].x)) + C2 * rg * (global_best_pos.x - particle_states[i].x);
+                particle_states[i].vely = (w * particle_states[i].vely + C1 * rp * (local_best_pos[i].y - particle_states[i].y)) + C2 * rg * (global_best_pos.y - particle_states[i].y);
+                if (particle_states[i].velx > V_MAX)
+                    particle_states[i].velx = V_MAX;
+                if (particle_states[i].vely > V_MAX)
+                    particle_states[i].vely = V_MAX;
+                if (particle_states[i].velx < -V_MAX)
+                    particle_states[i].velx = -V_MAX;
+                if (particle_states[i].vely < -V_MAX)
+                    particle_states[i].vely = -V_MAX;
 
                 particle_states[i].x = particle_states[i].x + particle_states[i].velx;
                 particle_states[i].y = particle_states[i].y + particle_states[i].vely;
-                // if (particle_states[i].x > upper_boundary)
-                //     particle_states[i].x = upper_boundary;
-                // if (particle_states[i].y > upper_boundary)
-                //     particle_states[i].y = upper_boundary;
-                // if (particle_states[i].x < lower_boundary)
-                //     particle_states[i].x = lower_boundary;
-                // if (particle_states[i].y < lower_boundary)
-                //     particle_states[i].y = lower_boundary;
+                // if (particle_states[i].x > UPPER_BOUNDARY)
+                //     particle_states[i].x = UPPER_BOUNDARY;
+                // if (particle_states[i].y > UPPER_BOUNDARY)
+                //     particle_states[i].y = UPPER_BOUNDARY;
+                // if (particle_states[i].x < LOWER_BOUNDARY)
+                //     particle_states[i].x = LOWER_BOUNDARY;
+                // if (particle_states[i].y < LOWER_BOUNDARY)
+                //     particle_states[i].y = LOWER_BOUNDARY;
 
                 // ==============================================================================================================================================================//
 
                 // OBSTACLE AVOIDANCE MODULE
-                final_val = obstacle_avoidance(num_obstacle, particle_states[i], global_best_pos, center_x, center_y, r, thresh, seed, lower_boundary, upper_boundary);
+                final_val = obstacle_avoidance(NUM_OBSTACLE, particle_states[i], global_best_pos, center_x, center_y, R, thresh, seed, LOWER_BOUNDARY, UPPER_BOUNDARY);
                 //===================================================================================================//
                 ct--;
 
@@ -254,23 +239,23 @@ int main()
         //For each particle evaluate fitness
         //If fitness(xt) > fitness(gbest) then gbest=xt
         //If fitness(xt) > fitness(pbest) then pbest=xt
-        for (int i = swarm_size; i--;)
+        for (int i = SWARM_SIZE; i--;)
         {
             //Set Particle Fitness
-            particle_fitness[i] = fitness(particle_states[i], target, global_pos[iter_ctr - 1]);
+            particle_fitness[i] = fitness(particle_states[i], global_pos[iter_ctr - 1]);
 
             // Updating local/particle best position
-            if (fitness(particle_states[i], target, global_pos[iter_ctr - 1]) < local_best_fit[i])
+            if (particle_fitness[i] > local_best_fit[i])
             {
                 local_best_pos[i].x = particle_states[i].x;
                 local_best_pos[i].y = particle_states[i].y;
-                local_best_fit[i] = fitness(local_best_pos[i], target, global_pos[iter_ctr - 1]);
+                local_best_fit[i] = fitness(local_best_pos[i], global_pos[iter_ctr - 1]);
                 // Updating global best position
-                if (local_best_fit[i] < global_best_fit)
+                if (local_best_fit[i] > global_best_fit)
                 {
                     global_best_pos.x = local_best_pos[i].x;
                     global_best_pos.y = local_best_pos[i].y;
-                    global_best_fit = fitness(global_best_pos, target, global_pos[iter_ctr - 1]);
+                    global_best_fit = fitness(global_best_pos, global_pos[iter_ctr - 1]);
 
                     global_best_pos.velx = particle_states[i].velx;
                     global_best_pos.vely = particle_states[i].vely;
@@ -288,7 +273,7 @@ int main()
             if (((global_pos[iter_ctr].x - global_pos[iter_ctr - 1].x) < 0.2) && ((global_pos[iter_ctr].x - global_pos[iter_ctr - 2].x) < 0.2) && ((global_pos[iter_ctr].x - global_pos[iter_ctr - 3].x) < 0.2) && ((global_pos[iter_ctr].y - global_pos[iter_ctr - 1].y) < 0.2) && ((global_pos[iter_ctr].y - global_pos[iter_ctr - 2].y) < 0.2) && ((global_pos[iter_ctr].y - global_pos[iter_ctr - 3].y) < 0.2))
             {
                 cout << "Local Minima detected: " << endl;
-                // Implementing Shortest Path Planning PSO
+                // Implementing Local Minima avoidance from "Shortest Path Planning PSO"
                 // (Applies a slight perturbation to the Global best particles so that it doesn't get stuck in a local minima.
                 //  The particles are updated by the given velocity formula)
                 //Check if particles are within boundary
@@ -304,13 +289,13 @@ int main()
                     Coord gbest;
                     gbest.x = global_best_pos.x + global_best_pos.velx;
                     gbest.y = global_best_pos.y + global_best_pos.vely;
-                    double gfit = fitness(gbest, target, global_pos[iter_ctr - 1]);
+                    double gfit = fitness(gbest, global_pos[iter_ctr - 1]);
                     global_best_pos = gbest;
                     global_best_fit = gfit;
 
                     //===================================================================================================//
                     // OBSTACLE AVOIDANCE MODULE::
-                    final_val = obstacle_avoidance(num_obstacle, global_best_pos, global_best_pos, center_x, center_y, r, thresh, seed, lower_boundary, upper_boundary);
+                    final_val = obstacle_avoidance(NUM_OBSTACLE, global_best_pos, global_best_pos, center_x, center_y, R, thresh, seed, LOWER_BOUNDARY, UPPER_BOUNDARY);
                     //===================================================================================================//
 
                     cs--;
@@ -323,13 +308,13 @@ int main()
 
         // Check if converged on goal
         counter++;
-        if (shortfit(global_best_pos, target) <= 150.0)
+        if (shortfit(global_best_pos, target) <= TARGET_TOLERANCE)
         {
             cout << "\n\nReached near goal. SUCCESS " << endl;
             flag = 0;
             break;
         }
-        else if (iter_ctr >= (no_of_iters / 20))
+        else if (iter_ctr >= LOCAL_CONV_TOLERANCE)
         {
             if (((global_pos[iter_ctr].x - global_pos[iter_ctr - 1].x) < 0.1) && ((global_pos[iter_ctr].x - global_pos[iter_ctr - 2].x) < 0.1) && ((global_pos[iter_ctr].x - global_pos[iter_ctr - 3].x) < 0.1) && ((global_pos[iter_ctr].y - global_pos[iter_ctr - 1].y) < 0.1) && ((global_pos[iter_ctr].y - global_pos[iter_ctr - 2].y) < 0.1) && ((global_pos[iter_ctr].y - global_pos[iter_ctr - 3].y) < 0.1))
             {
@@ -370,12 +355,19 @@ int main()
          << "Final Global Best Position: (" << global_best_pos.x << "," << global_best_pos.y << ")" << endl;
 
     // Writing to csv all the data for plotting
-    write_to_csv(particle_pos_x, particle_pos_y, global_pos, local_pos_x, local_pos_x, swarm_size, counter, center_x, center_y, r, num_obstacle, path_coord);
+    write_to_csv(particle_pos_x, particle_pos_y, global_pos, local_pos_x, local_pos_x, SWARM_SIZE, counter, center_x, center_y, R, NUM_OBSTACLE, path_coord);
     cout << "Run Successful" << endl;
-
-    return 0;
 }
 
+int main(int argc, char **argv)
+{
+    //Initial and Control Parameters
+    srand(time(0));
+    clock_t startTime = clock();
+    cout << "Initiating Optimization ..." << endl;
+    particleSwarm(argc, argv);
+    cout << "Optimization Completed in " << (float)startTime; //- startTime <<"seconds" << endl;
+}
 // Pseudo code
 /*
  Start
